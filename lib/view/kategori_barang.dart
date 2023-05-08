a148f224cd28a4ab101067da525bf0034cb4c1c3
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-05-08 17:40:01 +0700

    add code for view(kategori_barang.dart)

---
+++ lib/view/kategori_barang.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api/controller/kategori_barang_controller.dart';
+import 'package:flutter_api/model/kategori_barang_model.dart';
+
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  List<KategoriBarangModel> listKategoriBarang = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getKategoriBarang() async {
+    final kategoriBarang = await kategoriBarangController.getKategoriBarang();
+    setState(() {
+      listKategoriBarang = kategoriBarang;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
